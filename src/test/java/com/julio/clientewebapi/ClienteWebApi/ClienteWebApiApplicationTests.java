15f67f8c2484553fa72c5c11a836c3b0af85f1eb
unknown <juliomartelosso@gmail.com>
2021-04-11 17:25:25 +0200

    Initial commit

---
+++ src/test/java/com/julio/clientewebapi/ClienteWebApi/ClienteWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julio.clientewebapi.ClienteWebApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
